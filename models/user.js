8b61929049eca36edc4b944316d83635bc5dbeaf
kacem <kacemmeherzi52@gmail.com>
2024-02-29 22:42:15 +0100

    collation added to the user model

--- models/user.js
+++ models/user.js
@@ -1,17 +1,20 @@
 const mongoose = require('mongoose')  ;
-const bcrypt = require('bcryptjs') ; 
+const bcrypt = require('bcryptjs') ;
+
+
 const userschema = new mongoose.Schema ( {
-    username : {type : string ,unique  : true } ,password : String 
-},{collection :"users"}
+    username : {type : String ,unique  : true,required :true } ,password : {type :String, unique: false, required  : true} 
+},{collation: "users", collation: { locale: 'en_US', strength: 1 } }
 )
 
-userschema.pre('save' , async function (next ){
+userschema.pre('save' , async function (next){
+const salt = await bcrypt.genSalt(10);
 
 const user = this ; 
-if ( user.isModified("password")){
-    user.password = await bcrypt.hash(user.password) ;
+if (user.isModified("password")){
+    user.password = await bcrypt.hash(user.password,salt) ;
+    next();
 }
-next();
 
 
 })
