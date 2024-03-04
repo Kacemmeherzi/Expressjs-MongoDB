d8b60561098a41607d462afa65580ea29e8dca62
kacem <kacemmeherzi52@gmail.com>
2024-03-04 15:58:18 +0100

    auther added to post model

--- models/post.js
+++ models/post.js
@@ -3,7 +3,8 @@ const bcrypt = require('bcryptjs') ;
 
 
 const postshema = new mongoose.Schema ( {
-    title : {type : String ,unique  : false,required :true } ,body : {type :String, unique: false, required  : true} 
+    title : {type : String ,unique  : false,required :true } ,body : {type :String, unique: false, required  : true} ,
+    author : {type : mongoose.Schema.Types.ObjectId, ref : "User"}
 },{collation: "posts", collation: { locale: 'en_US', strength: 1 } }
 )
 const Post = mongoose.model ('Post',postshema) ;
